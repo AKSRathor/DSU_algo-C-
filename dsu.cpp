bab7aa4642a6a1270dc4d484443c73a12e13e0f4
AKSRathor <98573681+AKSRathor@users.noreply.github.com>
2024-06-02 13:10:58 +0530

    first commit

---
+++ dsu.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll N = 1e5;
+vector<ll>size(1e5, 0);
+vector<ll>parent(1e5, 0);
+void make(){
+    for(ll i =0; i<N; ++i){
+        size[i] = 1;
+        parent[i] = i;
+    }
+}
+ll find(ll vertex){
+    ll prt = parent[vertex];
+    if(prt == vertex){
+        return vertex;
+    }
+    return find(prt);
+}
+
+void union_func(ll a, ll b){
+    ll x = find(a);
+    ll y = find(b);
+    if(x!=y){
+        if(size[x]<size[y]){
+            swap(x, y);
+            parent[y] = x;
+            size[x] += size[y];
+        }
+    }
+}
+
+int main(){
+
+    return 0;
+}
